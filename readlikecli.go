1cb68d982ce531a8b68030d7777755b090e515d3
ben <bliao@ilegendsoft.com>
2015-10-15 10:37:40 +0800

    and exit point

--- readlikecli.go
+++ readlikecli.go
@@ -4,44 +4,48 @@ import (
 "github.com/chzyer/readline"
 	"strings"
 )
-func startTerminal(action *func(),commands []cli.Command,readlineConfig *readline.Config){
-	action=func(c *cli.Context) {
-		items:=make([] *readline.PrefixCompleter,10)
-		items=append(items,readline.PcItem("help"));
-		for _,cmd:=range commands{
-			items=append(items,readline.PcItem(cmd.Name))
+var on bool=true
+func Exit(){
+	on=false
+}
+func StartSession(commands []cli.Command,readlineConfig *readline.Config,usage string,version string){
+	items:=make([] *readline.PrefixCompleter,10)
+	items=append(items,readline.PcItem("help"));
+	for _,cmd:=range commands{
+		items=append(items,readline.PcItem(cmd.Name))
+	}
+	var completer = readline.NewPrefixCompleter()
+	completer.Children=items
+	if readlineConfig==nil{
+		readlineConfig=&readline.Config{
+			Prompt:">",
 		}
-		var completer = readline.NewPrefixCompleter(items)
-		completer
-		if readlineConfig==nil{
-			readlineConfig=&readline.Config{
-				Prompt:">",
-			}
-		}
-		readlineConfig.AutoComplete=completer
-		rl, err := readline.NewEx(readlineConfig)
+	}
+	readlineConfig.AutoComplete=completer
+	rl, err := readline.NewEx(readlineConfig)
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+	app:=cli.NewApp()
+	app.Usage=usage
+	app.Version=version
+	app.Commands=commands
+	for on==true{
+		line, err := rl.Readline()
 		if err != nil {
-			panic(err)
+			break
 		}
-		defer rl.Close()
-		app:=cli.NewApp()
-		app.Commands=commands
-		for {
-			line, err := rl.Readline()
-			if err != nil {
-				break
+		params:=strings.Split(line," ")
+		args:=[]string{app.Name}
+		for _,param:=range params{
+			trimed:=strings.TrimSpace(param)
+			if trimed!=""{
+				args=append(args,trimed)
 			}
-			params:=strings.Split(line," ")
-			args:=[]string{app.Name}
-			for _,param:=range params{
-				trimed:=strings.TrimSpace(param)
-				if trimed!=""{
-					args=append(args,trimed)
-				}
-			}
-			if len(args) >1 {
-				app.RunInside(args)
-			}
+		}
+		if len(args) >1 {
+			app.RunInside(args)
 		}
 	}
 
